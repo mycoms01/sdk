16f9e39878840559ce88d7780b33bfd395513411
sanit keawtawan <sanit.jigsaw@gmail.com>
2026-05-08 12:03:42 +0700

    feat: export constants from utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,4 @@ export { getChaiBuilderTailwindConfig } from "./get-chai-builder-tailwind-config
 export { getChaiBuilderTheme } from "./get-chai-builder-theme";
 
 export { chaiBuilderPlugin };
+export { COLLECTION_PREFIX, REPEATER_PREFIX } from "../core/constants/STRINGS";
